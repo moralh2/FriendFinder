2eec9c92c05d574a2427868f97605cfce4e58f6a
Hector M <moralh2@gmail.com>
2019-01-23 00:32:31 -0600

    route for post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,17 @@
 var friendData = require("../data/friend");
 
 // Export routes to server (need server reference in function)
-module.exports = function(app) {
+module.exports = function (app) {
 
     //Route to get Friends Data
-    app.get("/api/friends", function(request, response) {
+    app.get("/api/friends", function (request, response) {
         response.json(friendData);
-      });
-    
+    });
+
+    //Route to post Friends Data
+    app.post("/api/friends", function (request, response) {
+        friendData.push(request.body);
+        response.json(friendData);
+    });
+
 }
