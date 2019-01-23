b41443134e3a60e42d06feaf6f3501f20a33d9c0
Hector M <moralh2@gmail.com>
2019-01-23 02:23:46 -0600

    add more data, tweak to front-end

--- app/data/friend.js
+++ app/data/friend.js
@@ -4,9 +4,27 @@ var friends = [
         name:"Pepito",
         photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores:[
-            5,
             1,
-            4
+            1,
+            1
+          ]
+    },
+    {
+        name:"Pepita",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            3,
+            3,
+            3
+          ]
+    },
+    {
+        name:"Pepitote",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            5,
+            5
           ]
     }
 ];
