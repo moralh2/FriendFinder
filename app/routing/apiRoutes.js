ecf6213a209248d4ba4c1ce36e10c87d354b595e
Hector M <moralh2@gmail.com>
2019-01-23 04:24:58 -0600

    add questions w jquery, update data validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,8 +10,32 @@ module.exports = function (app) {
 
     //Route to post Friends Data
     app.post("/api/friends", function (request, response) {
+
+
+
+
+
+
+
+
         friendData.push(request.body);
+
+
+
+
         response.json(friendData);
     });
-    
+
 }
+
+// function calculateMatch(newFriend) {
+//     for (i = 0; i < friendData.length; i++) {
+
+//     }
+// }
+
+// function getDifference(array1, array2) {
+//     array1.map(function (currentValue, index) {
+//         return Math.abs(currentValue - array2[index])
+//     })
+// }
