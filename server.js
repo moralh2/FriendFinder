c613c16e8f66a96909d836642f0130a681b6703f
Hector M <moralh2@gmail.com>
2019-01-22 21:26:41 -0600

    add server

---
+++ server.js
@@ -0,0 +1,17 @@
+// Node Express Package
+var express = require("express");
+
+// Create Node server
+var app = express();
+
+// PORT 8080 unless Heroku
+var PORT = process.env.PORT || 8080;
+
+// Data parsing
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// Start server and listen on PORT
+app.listen(PORT, function(){
+    console.log("FriendFinder App Server is listening on PORT: " + PORT);
+});
